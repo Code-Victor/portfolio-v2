d4763d33c177813696b70869692f9e1495f85a66
Hamzat Victor <oluwaborihamzat@gmail.com>
2025-06-18 13:46:37 +0200

    fix: update site URL to the correct domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://hamz.at",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
