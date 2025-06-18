45ae5fa5430d87dc1f807a32d8d3cfab7b168bb8
Hamzat Victor <oluwaborihamzat@gmail.com>
2025-06-18 13:32:07 +0200

    feat: complete initial iteration
    - Updated favicon.svg with a new design.
    - Added avatar.png and og-image.png assets for user profile and social sharing.
    - Created Avatar.astro component to display user avatar with a styled border animation.
    - Introduced Texture.astro component for a noise effect background on the homepage.
    - Modified BaseHead.astro to use the new OG image for better social media previews.
    - Updated consts.ts with new site title and description.
    - Revamped index.astro to feature a personal introduction with avatar and texture effects.
    - Replaced global styles with Tailwind CSS for improved styling and responsiveness.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Hamzat Victor's Website";
+export const SITE_DESCRIPTION =
+  "A personal website for Hamzat Victor, a self-taught design engineer passionate about creating world class user experiences.";
